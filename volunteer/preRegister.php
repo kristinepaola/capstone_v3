e9cff9181aafecb313439b438d2022d209d2ea52
daphnemaine <33106234+daphnemaine@users.noreply.github.com>
2018-02-10 23:21:45 +0800

    dd

--- volunteer/preRegister.php
+++ volunteer/preRegister.php
@@ -87,21 +87,7 @@ echo json_encode($output);
 
 
 
-// check if na pre registered na ba
-// $check_query = "SELECT * FROM event_preregistration";
-// $check_data = mysqli_query($sql,$check_query);
-// $cnt = mysqli_num_rows($check_data);
-// $check_row = mysqli_fetch_array($check_data);
-// echo $cnt;
-// loop through event_preregistration table
 
-													
-// $select_query = "SELECT * FROM event_preregistration WHERE user_id = $user_id AND event_id = $event_id";
-// $select_data = mysqli_query($sql, $select_query);	
-
-// while($select_row = mysqli_fetch_assoc($select_data)){
-	// $output[] = $select_row;
-// }
 
 
 		
